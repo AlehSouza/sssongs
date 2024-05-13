1a0d2fe5b49494e513aed03e89e12088cdeaa888
Ale Souza <alexandrehg2001@gmail.com>
2024-05-13 18:32:34 -0300

    feat: adjusts in logo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { FooterComponent } from '../../components/footer/footer.component';
 import { ConverterComponent } from '../../components/converter/converter.component';
-import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, ConverterComponent, FooterComponent, NgFor],
+  imports: [ConverterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
